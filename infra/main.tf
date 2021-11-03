d884bf7dc5cd514cbd5972fd290322f6ffaf5b78
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-11-02 23:14:41 -0700

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,19 +3,12 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
-  }
-
-  backend "s3" {
-    bucket = "pradipta-s3-tfstate"
-    key    = "platform.tfstate"
-    region = "us-west-2"
-  }
+  } 
 }
-
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_ecr_repository" "ecr_devops_poc_pradipta" {
+resource "aws_ecr_repository" "ecr-devops-poc-pradipta" {
   name   = "ecr-devops-poc-pradipta"
 }
