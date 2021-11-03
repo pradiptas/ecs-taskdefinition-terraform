7768e579ca121c261f5a98eb56492a52430ec0f5
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-11-03 13:45:53 -0700

    Update main.tf
    update service config for Targets

--- infra/main.tf
+++ infra/main.tf
@@ -77,6 +77,12 @@ resource "aws_ecs_service" "ecs-devops-service" {
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
 
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}", "${aws_default_subnet.default_subnet_d.id}"]
     assign_public_ip = true # Providing our containers with public IPs
