26552273dedb4bb8ffa447923f7dc968206c7ab9
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-11-03 13:26:16 -0700

    Update main.tf
    service definition, subnets

--- infra/main.tf
+++ infra/main.tf
@@ -19,6 +19,9 @@ resource "aws_ecr_repository" "ecr-devops-pradipta" {
   name   = "ecr-devops-pradipta"
 }
 
+resource "aws_ecs_cluster" "ecs-devops-cluster" {
+  name = "ecs-devops-cluster" # Naming the cluster
+}
 
 resource "aws_ecs_task_definition" "ecs-devops-task" {
   family                   = "ecs-devops-task" # Naming our first task
@@ -65,4 +68,38 @@ data "aws_iam_policy_document" "assume_role_policy" {
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "ecs-devops-service" {
+  name            = "ecs-devops-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.ecs-devops-cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.ecs-devops-task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}", "${aws_default_subnet.default_subnet_d.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-west-2c"
+}
+
+resource "aws_default_subnet" "default_subnet_d" {
+  availability_zone = "us-west-2d"
 }
