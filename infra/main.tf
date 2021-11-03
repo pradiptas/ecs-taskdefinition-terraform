5b489573c3b546996a99e2983525ab560474d070
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-11-02 23:29:30 -0700

    cleanup for ecr

--- infra/main.tf
+++ infra/main.tf
@@ -3,12 +3,18 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
-  } 
-}
+  }
+    backend "s3" {
+    bucket = "pradipta-s3-tfstate"
+    key    = "platform.tfstate"
+    region = "us-west-2"
+  }
+} 
+
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_ecr_repository" "ecr-devops" {
-  name   = "ecr-devops"
+resource "aws_ecr_repository" "ecr-devops-pradipta" {
+  name   = "ecr-devops-pradipta"
 }
